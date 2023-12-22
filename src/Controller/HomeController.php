d7c0ff24aae2710cf799c5da9fd39e87234c2a43
Vladimir Boussekeyt <v.boussekeyt@ecoles-epsi.net>
2023-12-22 19:24:03 +0100

    commit avant recup de lapi plus simple

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Exception;
+use GuzzleHttp\Psr7\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,11 +18,27 @@ class HomeController extends AbstractController
         $api = new Api('food', 'fr');
         $product = $api->getProduct('737628064502');
 
+        dump($api);
+
+        dump($product);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'product' => $product,  // Ajoutez cette ligne pour définir la variable 'product'
+            'product' => $product,
         ]);
     }
+    #[Route('/product', name: 'product')]
+    public function ShowProduct(): Response
+    {
+        $api = new Api('food', 'fr');
+        $categories = $api->getCategories('Pastas');
+        dump($categories);
+
+        // $product_categorie = $api->getByFacets($categories);
+
+        return $this->render('home/product_show.html.twig', []);
+    }
+
+
 
     #[Route('/product/{code}', name: 'product_show')]
     public function productDetails($code): Response
@@ -31,11 +49,9 @@ class HomeController extends AbstractController
 
         dump($product);
 
-        $productImageUrl = $product->getImageFrontThumbUrl();
-
         return $this->render('home/product_details.html.twig', [
             'product' => $product,
-            'product_image_url' => $productImageUrl,
+
         ]);
     }
 }
